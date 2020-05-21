0b0e97902fdf8ba83d3c85d93d8f85b48836f54d
anverma <anusha.verma@soprasteria.com>
2020-05-21 19:47:11 +0530

    updated entities

--- ProductOffering/src/main/java/ProductOffering/entities/Order.java
+++ ProductOffering/src/main/java/ProductOffering/entities/Order.java
@@ -3,6 +3,9 @@ package ProductOffering.entities;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -11,17 +14,21 @@ import javax.persistence.Table;
 @Table(name="Order")
 public class Order {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="order_id")
-	private int OrderId;
+	private Integer OrderId;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name="id")
 	private User user;
 	
-	public int getOrderId() {
+	public Order() {}
+	
+	public Integer getOrderId() {
 		return OrderId;
 	}
-	public void setOrderId(int orderId) {
+	public void setOrderId(Integer orderId) {
 		OrderId = orderId;
 	}
 	public User getUser() {
--- ProductOffering/src/main/java/ProductOffering/entities/Product.java
+++ ProductOffering/src/main/java/ProductOffering/entities/Product.java
@@ -2,12 +2,17 @@ package ProductOffering.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="product")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="prod_id")
 	private int prodId;
 	
